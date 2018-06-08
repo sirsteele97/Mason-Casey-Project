9388e61553ba9508057d372eba756c1884d4c6ff
CaseyJr <CaseyJr@192.168.0.18>
2018-06-08 16:52:53 -0400

    viewer2

---
+++ ProjectMasonCasey5/src/main/java/com/MasonCasey/LogInPage.java
@@ -0,0 +1,48 @@
+package com.MasonCasey;
+
+import javax.annotation.PostConstruct;
+
+import com.MasonCasey.student.Student;
+import com.vaadin.data.Binder;
+import com.vaadin.event.MouseEvents.ClickEvent;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.shared.ui.ValueChangeMode;
+import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.TextField;
+
+@SpringView(name = MainView.VIEW_NAME)
+public class LogInPage implements View {
+
+
+public static final String VIEW_NAME = "LogIn Page";
+HorizontalLayout horizontal = new HorizontalLayout();
+	@PostConstruct
+	void init() {
+		
+	
+	Button joinButton = new Button("JOIN US!");
+	Button signinButton = new Button("SIGN IN!");
+	
+	horizontal.addComponent(joinButton);
+	horizontal.addComponent(signinButton);
+	
+	horizontal.setComponentAlignment(joinButton, Alignment.MIDDLE_LEFT);
+	horizontal.setComponentAlignment(signinButton, Alignment.MIDDLE_RIGHT);
+
+
+
+
+	
+}
+
+
+@Override
+public void enter(ViewChangeEvent event) {
+
+}
+}
